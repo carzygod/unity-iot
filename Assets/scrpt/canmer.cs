0255a3742a0475ad5eb1eb955cc3d686e1f180bb
carzygod <2373250856@qq.com>
2021-01-16 01:09:57 +0800

    🔥finally

--- Assets/scrpt/canmer.cs
+++ Assets/scrpt/canmer.cs
@@ -1,21 +1,29 @@
-﻿using UnityEngine;
-using BestHTTP;
-using BestHTTP.WebSocket;
-using UnityEngine.UI;
+﻿
 using System;
 using System.Text;
-
-using System.Net.WebSockets;
+using UnityEngine;
+using UnityEngine.UI;
+using BestHTTP.WebSocket;
+using System.Collections.Generic;
+//using Newtonsoft.Json.Linq;
+using System.Collections;
 using System.Threading;
+using SocketIO;
+//using System.Net.WebSockets;
+
 public class canmer : MonoBehaviour
 {
-
+public bool statu = false;
+public  GameObject light;
     // Start is called before the first frame update
     void Start()
     {
-      WebSocket();
-       
-        Debug.Log("初始化");
+       Init();
+       Connect();
+        Debug.Log("wph003初始化");
+         
+         //light.
+         light.SetActive (statu);
     }
 
     // Update is called once per frame
@@ -28,9 +36,20 @@ public class canmer : MonoBehaviour
             RaycastHit hitInfo;
             if(Physics.Raycast(ray,out hitInfo)){
                 GameObject gameObject = hitInfo.collider.gameObject;
-              sendWebSocket();
                     Debug.Log(gameObject.name);
-                    
+                    if(gameObject.name=="light"){
+                        if(statu){
+                        statu=false;
+                        light.SetActive (statu);
+                        Send("{\"target\":\"wph002\",\"msg\":\"off\"}");
+                        }else{
+                        statu=true;                            
+                        light.SetActive (statu);
+                        Send( "{\"target\":\"wph002\",\"msg\":\"on\"}");
+                        }
+
+                    }
+                  
             }
         }
 
@@ -38,41 +57,101 @@ public class canmer : MonoBehaviour
 
 //🔥websocket相关
 
-    public string urll = "ws://tmp.sidcloud.cn:33000";
+    public string url = "ws://tmp.sidcloud.cn:33000";
  
     public String msg =  "wph003";
  
-    public Text log;
+    //public string url = "ws://localhost:8080//websocket/";
  
-             ClientWebSocket ws = new ClientWebSocket();
-            CancellationToken ct = new CancellationToken();
-               public async void sendWebSocket()
-               {
- await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("wph003")), WebSocketMessageType.Binary, true, ct); //发送数据
-               }
-   public async void WebSocket()
+    private WebSocket webSocket;
+ 
+    private void Init()
     {
-        try
-        {
+        webSocket = new WebSocket(new Uri(url));
+        webSocket.OnOpen += OnOpen;
+        webSocket.OnMessage += OnMessageReceived;
+        webSocket.OnError += OnError;
+        webSocket.OnClosed += OnClosed;    
+    }
+ 
+    private void AntiInit()
+    {
+        webSocket.OnOpen = null;
+        webSocket.OnMessage = null;
+        webSocket.OnError = null;
+        webSocket.OnClosed = null;
+        webSocket = null;
+    }
+ 
+    public void Connect()
+    {
+        webSocket.Open();
+        print("发送连接");
+    }
+ 
+    private byte[] getBytes(string message)
+    {
+        byte[] buffer = Encoding.Default.GetBytes(message);
+        return buffer;
+    }
+    public void Send(string str)
+    {
+        webSocket.Send(str);
+    }
+ 
+    public void Close()
+    {
+        webSocket.Close();
+    }
+    void OnOpen(WebSocket ws)
+    {
+        Debug.Log("连接成功");    
+        //初始数据
+         Send("wph003"); 
+        
+    }
+   /// <summary>
+    /// 接收信息
+    /// </summary>
+    /// <param name="ws"></param>
+    /// <param name="msg">接收内容</param>
+    void OnMessageReceived(WebSocket ws,string msg)
+    {
+        Debug.Log(msg);
+    }
+    /// <summary>
+    /// 关闭连接
+    /// </summary>
+    void OnClosed(WebSocket ws, UInt16 code, string message)
+    {
+        Debug.Log(message);
 
-            //添加header
-            //ws.Options.SetRequestHeader("X-Token", "eyJhbGciOiJIUzI1N");
-            Uri url = new Uri(urll);
-            await ws.ConnectAsync(url, ct);
-            await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes("wph003")), WebSocketMessageType.Binary, true, ct); //发送数据
-            while (true)
-            {
-                var result = new byte[1024];
-                await ws.ReceiveAsync(new ArraySegment<byte>(result), new CancellationToken());//接受数据
-                var str = Encoding.UTF8.GetString(result, 0, result.Length);
-                Debug.Log(str);
-            }
-        }
-        catch (Exception ex)
+        Init();
+    }
+ 
+    private void OnDestroy()
+    {
+        if (webSocket != null && webSocket.IsOpen)
         {
-            Console.WriteLine(ex.Message);
+            webSocket.Close();
+
         }
     }
+   
+    /// <summary>
+    /// Called when an error occured on client side
+    /// </summary>
+    void OnError(WebSocket ws, Exception ex)
+    {
+        string errorMsg = string.Empty;
+#if !UNITY_WEBGL || UNITY_EDITOR
+        if (ws.InternalRequest.Response != null)
+            errorMsg = string.Format("Status Code from Server: {0} and Message: {1}",      ws.InternalRequest.Response.StatusCode, ws.InternalRequest.Response.Message);
+#endif
+        Debug.Log(errorMsg);
+        Init();
+    }
+ 
 
 
 }
